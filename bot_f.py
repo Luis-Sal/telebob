8ce004faa80a9863fd1bc6cdaaaff017be4803fc
Luis-Sal <73254663+Luis-Sal@users.noreply.github.com>
2021-05-27 19:05:02 +0200

    Update bot_f.py

--- bot_f.py
+++ bot_f.py
@@ -12,7 +12,7 @@ bot.
 """
 
 import logging
-
+import token_bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Enable logging
@@ -49,7 +49,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("1712365953:AAFvBbPob2dL7mdp9in8oyJOzPSYheTAwxI", use_context=True)
+    updater = Updater("token_bot.TOKEN", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
